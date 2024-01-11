13f15ea40d7bebf70887e6145c11da74f1794821
McleodHerritt <nmcleod2021@gmail.com>
2024-01-10 20:37:43 -0400

    Set up seed scrit that makes a connection to the database and seeds the database with the json data. .Added .env to the gitignore.

---
+++ server/seed/seed.js
@@ -0,0 +1,21 @@
+const path = require("path");
+require("dotenv").config({ path: "../../.env" });
+const mongoose = require("mongoose");
+const Questions = require("../models/Questions");
+const seedData = require("./seeds.json");
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/codinginterviewpro"
+);
+
+mongoose.connection.on("open", async () => {
+  try {
+    await Questions.deleteMany({});
+    await Questions.insertMany(seedData);
+    console.log("Database seeded successfully!");
+  } catch (error) {
+    console.error("Error seeding database:", error);
+  } finally {
+    mongoose.connection.close();
+  }
+});
