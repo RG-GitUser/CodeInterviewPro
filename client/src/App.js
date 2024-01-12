0aca5637f82cef9634e4c71eac30baae8470f1cf
McleodHerritt <nmcleod2021@gmail.com>
2024-01-11 20:32:48 -0400

    Added header component. Added Links. Add basic css styling. Add React Routes and Router to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./App.css";
-import { QuestionForm } from "./Components";
+import { QuestionForm, Header } from "./Components";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import {
   ApolloProvider,
   ApolloClient,
@@ -10,17 +12,41 @@ import {
 
 // Create an ApolloClient instance
 const client = new ApolloClient({
-  link: new HttpLink({ uri: "http://localhost:3001/graphql" }),
+  link: new HttpLink({ uri: "http://127.0.0.1:3001/graphql" }),
   cache: new InMemoryCache(),
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="App">
-        <QuestionForm />
-      </div>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/add-question" element={<AddQuestion />} />
+          </Routes>
+        </div>
+      </Router>
     </ApolloProvider>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <Header />
+      Home
+    </>
+  );
+}
+
+function AddQuestion() {
+  return (
+    <div>
+      <Header />
+      <h1>Add Question</h1>
+      <QuestionForm />
+    </div>
   );
 }
 
