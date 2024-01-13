af386409c36a3662f841291893b6ed313e88b7fb
RG-GitUser <rileygaffney3@icloud.com>
2024-01-12 21:31:09 -0400

    made changes to the server.js file and added comments

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,4 +1,6 @@
-const typeDefs = require ('./typeDefs');
+// schema/index.js
+
+const typeDefs = require('./typeDefs'); 
 const resolvers = require('./resolvers');
 
-module.exports ={typeDefs,resolvers}
+module.exports = { typeDefs, resolvers };
