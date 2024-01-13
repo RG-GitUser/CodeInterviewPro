41a197296e90a9a901d68955ca613a700a0794c8
RG-GitUser <rileygaffney3@icloud.com>
2024-01-12 21:03:04 -0400

    added chakra UI imports and function to line 59.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { ChakraProvider } from '@chakra-ui/react' //chakra UI import 
+import * as ReactDOM from 'react-dom/client'
+
 import "./App.css";
 import { QuestionForm, Header } from "./Components";
+
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import {
@@ -48,6 +52,40 @@ function AddQuestion() {
       <QuestionForm />
     </div>
   );
+}
+
+const app = createApp(App);
+
+// Install Chakra UI
+const rootElement = document.getElementById('root')
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}> {/* <--- Extend theme to handle  */ }
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+)
+
+import {
+  ChakraBaseProvider,
+  extendBaseTheme,
+  theme as chakraTheme,
+} from '@chakra-ui/react'
+
+const { Button } = chakraTheme.components
+
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+})
+
+function App() {
+  return (
+    <ChakraBaseProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraBaseProvider>
+  )
 }
 
 export default App;
