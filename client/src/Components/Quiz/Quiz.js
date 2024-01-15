13316142b85b33d9b61cf519f61ce933ceac2ebe
McleodHerritt <nmcleod2021@gmail.com>
2024-01-15 18:11:00 -0400

    added arrows to allow navagating throught the quiz questions. Styled arrows, changed style of the flip-card

--- client/src/Components/Quiz/Quiz.js
+++ client/src/Components/Quiz/Quiz.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./quiz.css";
 import { FlipCard } from "../../Components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const Quiz = ({ questions }) => {
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  const goToNextQuestion = () => {
+    if (currentQuestionIndex === questions.length - 1) {
+      return;
+    }
+    setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const goToPreviousQuestion = () => {
+    if (currentQuestionIndex === 0) {
+      return;
+    }
+    setCurrentQuestionIndex((prevIndex) => prevIndex - 1);
+  };
+
+  const currentQuestion = questions[currentQuestionIndex];
+
   return (
     <div>
       <div className="quiz">
-        {questions.map((question) => (
-          <FlipCard key={question.id} question={question.question} answer={question.answer} />
-        ))}
+        <FlipCard key={currentQuestion.id} question={currentQuestion.question} answer={currentQuestion.answer} />
+      </div>
+      <div className="buttons">
+        <button onClick={goToPreviousQuestion}>
+          <FontAwesomeIcon icon={faArrowLeft} />
+        </button>
+        <button onClick={goToNextQuestion}>
+          <FontAwesomeIcon icon={faArrowRight} />
+        </button>
       </div>
     </div>
   );
