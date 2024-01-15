c980071d742aada60e982db78ac1b2ee40afd113
Matt Thompson <matthewthompson118@gmail.com>
2024-01-15 00:57:44 -0400

    modified FlipCard.js to integrate properly with BackEnd

--- client/src/Components/FlipCard.js
+++ client/src/Components/FlipCard.js
@@ -1,28 +1,50 @@
-import React, {useState } from 'react';
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from '@apollo-client'
 import "./FlipCard.css"; // Importing the CSS file
 
-const FlipCard= ({ question, answer }) => {
-    const [isflipped, setisFlipped] = useState(false);
+const GET_QUESTION = gql`
+    query {
+        getAllQuestions {
+            id
+            question
+            answer
+        }
+    }
+`;
 
-    const handleFlip= () => {
-        setisFlipped(!flipped);
-    };
+const FlipCard = () => {
+    const { loading, error, data } = useQuery(GET_QUESTION);
 
     return (
-       <div
-        className={`flip-card ${isflipped ? "flipped" : ""}`}
-        onClick={handleFlip}
-       >
-        <div className="flip-card-inner">
-            <div className="flip-card-front">
-                <p>{question}</p>
-            </div>
-            <div className="flip-card-back">
-                <p>{answer}</p>
-            </div>
-         </div>
-       </div>
+        <div className="flip-card">
+            {loading ? (
+                <div className="card question-card">
+                    Loading...
+                </div>
+            ) : error ? (
+                <div className="card question-card">    
+                    Error fetching questions
+                </div>
+            ) : (
+                data.getAllQuestions.map((question) => (
+                    <div key={question.id} className="card question-card">
+                        <div className="card-inner">
+                            <div className="card-front">
+                                <p>Click to view question</p>
+                            </div>
+                            <div className="card-back">
+                                <p>{question.question}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))
+            )}
+        </div>
     );
 };
+                
+    
+
 
 export default FlipCard;
--- client/src/apollo.js
+++ client/src/apollo.js
@@ -1,10 +1,19 @@
 // apollo.js
-
+import React from 'react';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'YOUR_GRAPHQL_API_ENDPOINT', 
   cache: new InMemoryCache(),
 });
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      {/* component code */ }
+    </ApolloProvider>
+      
+  );
+}
 
 export default client;
