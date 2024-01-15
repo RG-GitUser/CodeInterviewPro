656639ff255c21a9fbcb5f1d9311d8a5064b6d2b
Matt Thompson <matthewthompson118@gmail.com>
2024-01-14 20:53:21 -0400

    modified functionality of flipcard.js

--- client/src/Components/FlipCard.js
+++ client/src/Components/FlipCard.js
@@ -1,24 +1,27 @@
 import React, {useState } from 'react';
 import "./FlipCard.css"; // Importing the CSS file
 
-const FlipCard= ({ question }) => {
-    const [flipped, setFlipped] = useState(false);
+const FlipCard= ({ question, answer }) => {
+    const [isflipped, setisFlipped] = useState(false);
 
-    const handleClick = () => {
-        setFlipped(!flipped);
+    const handleFlip= () => {
+        setisFlipped(!flipped);
     };
 
     return (
-        <div className='flip-card' onCLick={handleClick}>
-            <div className={`flip-card-inner ${flipped ? 'flipped' : ''}`}> 
-                <div className='flip-card-front'>   
-                    <p>Click to view the question</p>
-                </div>
-                <div className='flip-card-back'>
-                    <p>{question}</p>
-                </div>
+       <div
+        className={`flip-card ${isflipped ? "flipped" : ""}`}
+        onClick={handleFlip}
+       >
+        <div className="flip-card-inner">
+            <div className="flip-card-front">
+                <p>{question}</p>
             </div>
-        </div>
+            <div className="flip-card-back">
+                <p>{answer}</p>
+            </div>
+         </div>
+       </div>
     );
 };
 
