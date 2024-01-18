24fbba503d270dfc50ba3d5f95658034e22188c4
RG-GitUser <rileygaffney3@icloud.com>
2024-01-18 15:04:22 -0400

    adjusted about us page

--- client/src/Components/About/about.jsx
+++ client/src/Components/About/about.jsx
@@ -4,24 +4,25 @@ import Header from '../Header/Header';
 import './about.css';
 
 const About = () => {
-  return (
-    <div>
+    return (
+      <div className="about-container">
         <Header />
-      <h1>About Us</h1>
-      <p>
-        Welcome to our website! We are a team of passionate individuals dedicated to providing
-        high-quality content and services.
-      </p>
-      <p>
-        Our mission is to equip newly graduated Full-Stack Web Development students with the knowledge they need to succeed in their interviews.
-         Whether you are a visitor or a contributor, we value your presence and participation in our community.
-      </p>
-      <p>
-        If you have any questions or suggestions, feel free to send us a <Link className ='msgLink' to="/contact">Message!</Link>{' '}
-      </p>
-    
-    </div>
-  );
-};
-
-export default About;
+        <div className="about-content">
+          <h1>About Us</h1>
+          <p>
+            Welcome to our Coding Interview Pro! We are a team of passionate individuals dedicated to providing
+            high-quality content and services.
+          </p>
+          <p>
+            Our mission is to equip newly graduated Full-Stack Web Development students with the knowledge they need to succeed in their interviews.
+            Whether you are a visitor or a contributor, we value your presence and participation in our community.
+          </p>
+          <p>
+            If you have any questions or suggestions, feel free to send us a <Link className='msgLink' to="/contact">Message!</Link>{' '}
+          </p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default About;
